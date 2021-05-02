8bfb6288a30c3ab190cede0ff3ba92d0ff75c36b
Maitri96 <81248328+Maitri96@users.noreply.github.com>
2021-05-02 19:25:29 +0530

    Add files via upload

---
+++ Capstone Project Notebook.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#  ## Capstone Project Notebook
+#  
+#  This notebook will be mainly used for capstone project.
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
